4fc53accb60ad7c446791a6c75460bc52bc7e5d8
Damjan Pericic <d.pericic@vega.local>
2019-07-04 09:28:15 +0200

    Initial Commit

---
+++ android/app/src/main/java/com/rnchatkitdemo/MainApplication.java
@@ -0,0 +1,62 @@
+package com.rnchatkitdemo;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.brentvatne.react.ReactVideoPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.rnfs.RNFSPackage;
+import com.reactnativedocumentpicker.ReactNativeDocumentPicker;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.futurice.rctaudiotoolkit.AudioPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+import com.reactnativecommunity.rctaudiotoolkit.AudioPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RNFetchBlobPackage(),
+            new ReactVideoPackage(),
+            new VectorIconsPackage(),
+            new RNGestureHandlerPackage(),
+            new RNFSPackage(),
+            new ReactNativeDocumentPicker(),
+            new ReactNativeConfigPackage(),
+            new AudioPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
